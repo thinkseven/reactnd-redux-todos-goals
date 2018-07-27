7bd58d063ce49e3efe58abd9869257e7dab0a4e6
Vinod Chandra <vinod.chandra@homesite.com>
2018-07-27 11:57:03 -0400

    store with 1 reducer

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Most of the commits in this repository correspond to videos in the program.
 
 ## Project Setup
 
-1. Clone the Project - git clone https://github.com/udacity/reactnd-redux-todos-goals.git
-2. Go into the directory where the project now lives - `cd reactnd-redux-todos-goals`
-3. Install the dependencies - `npm install`
-4. Start the app - `npm start`
+1.  Clone the Project - git clone https://github.com/udacity/reactnd-redux-todos-goals.git
+2.  Go into the directory where the project now lives - `cd reactnd-redux-todos-goals`
+3.  Install the dependencies - `npm install`
+4.  Start the app - `npm start`
 
 ## Contributing
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore () {
+// Library Code
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -6,10 +7,102 @@ function createStore () {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = listener => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter(l => l != listener)
+    }
+  }
+
+  const dispatch = action => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
   return {
-    getState
+    getState,
+    subscribe,
+    dispatch,
   }
 }
+
+// App Code
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todos])
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id != action.id)
+    case 'TOGGLE_TODO':
+      return state.map(
+        todo =>
+          todo.id != action.id
+            ? todo
+            : Object.assign({}, todo, { complete: !todo.complete }),
+      )
+    default:
+      return state
+  }
+}
+
+const store = createStore(todos)
+
+const unsubcriber1 = store.subscribe(() => {
+  console.log('Subcribe1: The new state is: ', store.getState())
+})
+// unsubcriber1()
+
+const unsubcriber2 = store.subscribe(() => {
+  console.log('Subcribe2: The new state is: ', store.getState())
+})
+// unsubcriber2()
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todos: {
+    id: 1,
+    name: 'finish redux',
+    complete: false,
+  },
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todos: {
+    id: 2,
+    name: 'finish react',
+    complete: false,
+  },
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todos: {
+    id: 3,
+    name: 'have doctor appointment',
+    complete: false,
+  },
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todos: {
+    id: 4,
+    name: 'visit india',
+    complete: false,
+  },
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 4,
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 3,
+})
